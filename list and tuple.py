f5168fb8e01319ba90099c482f402f10fa20fa53
fonaldinho-19 <1959828263@qq.com>
2026-04-22 22:37:33 +0800

    Add files via upload

---
+++ list and tuple.py
@@ -0,0 +1,25 @@
+"""
+请用索引取出下面list的指定元素：
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Bob']
+]
+
+# 打印Apple:
+print(?)
+# 打印Python:
+print(?)
+# 打印Bob:
+print(?)
+"""
+#-*- coding: utf-8 -*-
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Bob']
+]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
