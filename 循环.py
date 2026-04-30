1d44e024d4ba694a83d7893c47985e1b0be8c9d8
fonaldinho-19 <1959828263@qq.com>
2026-04-30 23:20:48 +0800

    Add files via upload

---
+++ 循环.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+"""
+请利用循环依次对list中的每个名字打印出Hello, xxx!：
+
+L = ['Bart', 'Lisa', 'Adam']
+"""
+List = ['Bart','Lisa','Adam']
+for x in List:
+    print('HELLO,%s!'%x)
+"""
+利用while，for循环以及break，continue写一个死循环程序.
+"""
+n = 1
+while n > 0:
+    print(n)
+    n +=1
+
+
